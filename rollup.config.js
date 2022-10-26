7e14a96198dd7c04431cda53fa268030c02f5ec9
codingJJJ <271533323@qq.com>
2022-10-26 16:34:43 +0800

    feat: 配置rollup build

---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+const babel = require('rollup-plugin-babel')
+const pkg = require('./package.json')
+
+const enableSourcemap = true;
+const extensions = [".ts", ".js", ".json", ".node"];
+const buildBaseConfig = {
+  input: 'lib/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      sourceMap: enableSourcemap,
+      name: 'RuleEngine'
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: enableSourcemap
+    },
+    {
+      file: pkg.commonjs,
+      format: 'cjs',
+      sourcemap: enableSourcemap,
+      exports: "auto"
+    }
+  ],
+  watch: {
+    include: "lib/**",
+  },
+  plugins: [
+    typescript({
+      useTsconfigDeclarationDir: true
+    }),
+    commonjs(),
+    resolve({extensions}),
+    sourcemaps(),
+    terser(),
+    babel({
+      exclude: 'node_modeles/**',
+      extensions
+    })
+  ]
+};
+
+const dtsBaseConfig = {
+  input: 'lib/index.ts',
+  output: [{
+    file: pkg.types,
+    format: 'es',
+    exports: "auto"
+  }],
+  plugins: [dts.default()]
+}
+
+export default [buildBaseConfig, dtsBaseConfig]
